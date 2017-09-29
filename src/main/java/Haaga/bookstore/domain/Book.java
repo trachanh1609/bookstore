9361ed51b63e79e5edeebd14114d86e93f91a8d2
Vu Nguyen <vunguyen@Vus-MacBook-Air.local>
2017-09-29 14:46:41 +0300

    add REST services for books, categories

--- src/main/java/Haaga/bookstore/domain/Book.java
+++ src/main/java/Haaga/bookstore/domain/Book.java
@@ -1,11 +1,13 @@
 package Haaga.bookstore.domain;
 
 import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Book {
@@ -18,6 +20,7 @@ public class Book {
     private String year;
     
     @ManyToOne
+    @JsonIgnore  
     @JoinColumn(name = "categoryid")
     private Category category;
     
--- src/main/java/Haaga/bookstore/web/BookStoreController.java
+++ src/main/java/Haaga/bookstore/web/BookStoreController.java
@@ -1,11 +1,14 @@
 package Haaga.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import Haaga.bookstore.domain.Book;
 import Haaga.bookstore.domain.BookRepository;
@@ -50,4 +53,14 @@ public class BookStoreController {
     		model.addAttribute("categories", crepository.findAll());
     		return "editBook";
     	}
+    
+    @RequestMapping(value="books", method=RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest(){
+    		return (List<Book>) repository.findAll(); 
+    }
+    
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {	
+    		return repository.findOne(bookId);
+    }
 }
