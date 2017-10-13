b5a20ea01631c510a89058fbbc21b8b9f7145aa2
trachanh1609 <Vu@Users-MacBook-Pro.local>
2017-10-13 14:45:49 +0300

    add test, cors

--- src/test/java/Haaga/bookstore/BookstoreApplicationTests.java
+++ src/test/java/Haaga/bookstore/BookstoreApplicationTests.java
@@ -2,15 +2,23 @@ package Haaga.bookstore;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.*;
+
+import Haaga.bookstore.web.BookStoreController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookstoreApplicationTests {
+	
+	@Autowired
+	private BookStoreController controller;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception{
+		assertThat(controller).isNotNull();
 	}
 
 }
